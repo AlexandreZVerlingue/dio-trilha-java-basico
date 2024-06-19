7798d2a794285d1180f17c258cd2a1c197427880
AlexandreZVerlingue <alexandrezanquim@gmail.com>
2024-06-18 22:58:53 -0300

    Aprendendo sobre tipos e variáveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        String texto = "Um texto qualquer!";
+        String espaco = "  ";
+        int idade = 30;
+        double salario = 2300.56;
+        final int ANO_IMUTAVEL = 2900;
+        System.out.print(idade);
+        System.out.print(espaco);
+        System.out.print(salario);
+        System.out.print(espaco);
+        System.out.print(ANO_IMUTAVEL);
+        System.out.print(espaco);
+        System.out.print(texto);
+
+    }
+}
